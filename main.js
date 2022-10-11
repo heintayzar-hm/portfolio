67e9133392d63430c2dcbbca8facd5d6dd14bb60
Hein Tay Zar <heintayzarhm@gmail.com>
2022-10-11 20:59:59 +0630

    Add work section and make it dynamanic

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const projects = [
       big: "A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required."
     },
     technologies: ["HTML", "CSS", "Ruby on Rails"],
-    links: ["github pages", "heroku"],
+    links: ["https://github.com", "https://heroku.com"],
   },
   {
     id: "1",
@@ -20,8 +20,8 @@ const projects = [
       small: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
       big: "A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required."
     },
-    technologies: ["HTML", "CSS", "Ruby on Rails"],
-    links: ["github pages", "heroku"],
+    technologies: ["HTML", "CSS", "JavaScript"],
+    links: ["https://github.com", "https://heroku.com"],
   },
   {
     id: "2",
@@ -33,7 +33,7 @@ const projects = [
       big: "A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required."
     },
     technologies: ["HTML", "CSS", "Ruby on Rails"],
-    links: ["github pages", "heroku"],
+    links: ["https://github.com", "https://heroku.com"],
   },
   {
     id: "3",
@@ -45,7 +45,7 @@ const projects = [
       big: "A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required."
     },
     technologies: ["HTML", "CSS", "Ruby on Rails"],
-    links: ["github pages", "heroku"],
+    links: ["https://github.com", "https://heroku.com"],
   }
 ];
 
@@ -54,8 +54,62 @@ const crossBtn = document.querySelector('#cross-btn');
 const mobileMenu = document.querySelector('#mobile-menu');
 const popup = document.querySelector('.pop-up');
 const mobileLinks = document.querySelectorAll('.mobile-link');
-const btnProjects = document.querySelectorAll('.btn-project');
 const workPortfolio = document.querySelector('#portfolio');
+const divPopup = document.createElement('div');
+
+//work section
+let listProject ='';
+let listOfelements = '';
+
+projects.forEach((project)=>{
+  listOfelements='';
+  project.technologies.forEach((technologie) => {
+    listOfelements += '<li class="card-tag"><span class="card-tag-text">' + technologie + '</span></li>';
+    console.log(listOfelements)
+  })
+  listProject += `<li class="card">
+  <img class="snap-img-class " src="${project.imgsrc}" alt="fake1">
+  <div class="card-block">
+      <div class="card-header">
+          <h2 class="card-header-text">${project.title}</h2>
+          <div class="card-header-frame">
+              <div class="card-header-frame-client">
+                  <span class="card-header-frame-client-text">${project.tags[0]}</span>
+              </div>
+              <span class="counter">
+                  <img src="resources/Counter.svg" alt="counter"> 
+              </span>
+              <div class="card-header-frame-role">
+                  <span class="card-header-frame-role-text">${project.tags[1]}</span>
+              </div>
+              <span class="counter">
+                  <img src="resources/Counter.svg" alt="counter"> 
+              </span>
+              <div class="card-header-frame-year">
+                  <span class="card-header-frame-year-text">${project.tags[2]}</span>
+              </div>
+          </div>
+      </div>
+      <p class="card-description"> A daily selection of privately personalized reads; no accounts or sign-ups required.</p>
+      <ul class="card-tags">
+          ${listOfelements}
+      </ul>
+      <div class="resume translate-bg">
+          <button type="button" class="button-link px-12 mx-24 translate-button btn-project"><span class="button-link-text">See Project</span></button>
+      </div>
+  </div>
+</li>`
+})
+workPortfolio.innerHTML= `<ul class="main-card-container">
+${listProject}
+
+</ul>`
+
+
+
+
+
+
 
 hamburgerBtn.addEventListener('click', () => {
   mobileMenu.style.display = 'block';
@@ -77,16 +131,17 @@ mobileLinks.forEach(
   },
 );
 
-let listOfelements = '';
+const btnProjects = document.querySelectorAll('.btn-project');
 
-btnProjects.forEach(
+btnProjects.forEach(  
   (btnProject, i) => {
+    console.log(btnProject)
     btnProject.addEventListener('click', () => {
-      console.log(projects[i].title);
-      {projects[i].technologies.forEach((technologie) => {
+      listOfelements = '';
+      projects[i].technologies.forEach((technologie) => {
         listOfelements += '<li class="card-tag"><span class="card-tag-text">' + technologie + '</span></li>';
-      })}
-      const divPopup = document.createElement('div');
+        console.log(listOfelements)
+      })
       divPopup.classList.add('pop-up');
       divPopup.innerHTML = ` <div class="pop-up-card">
 <div class="pop-up-header">
@@ -124,10 +179,10 @@ btnProjects.forEach(
         ${listOfelements}
         </ul>
         <div class="pop-up-buttons">
-            <button type="button" class="button-link w-146 translate-button"><a href="#" class="button-link-text btn-flex">See Live
+            <button type="button" class="button-link w-146 translate-button"><a target="_blank" href="${projects[i].links[0]}" class="button-link-text btn-flex">See Live
             <img class="p-3" src="resources/live_show.svg" alt="live_icon">
         </a>  </button>
-            <button type="button" class="button-link w-146 translate-button"><a  href="#" class="button-link-text btn-flex">See Source
+            <button type="button" class="button-link w-146 translate-button"><a target="_blank" href="${projects[i].links[1]}" class="button-link-text btn-flex">See Source
                 <img class="p-3" src="resources/github_icon.svg" alt="live_icon">
             </a>
             </button>
@@ -144,6 +199,7 @@ btnProjects.forEach(
       const popupCloseBtn = document.querySelector('#pop-up-cross');
       popupCloseBtn.addEventListener('click', () => {
         divPopup.style.display = 'none';
+        divPopup.innerHTML = '';
       })
 
     })
